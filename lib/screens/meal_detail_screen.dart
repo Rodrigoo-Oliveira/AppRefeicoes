301c59f66f318fd72d6a9477a081a0060ba8d2f1
Rodrigo Oliveira <rodrigoodeoliveira.santos@gmail.com>
2021-09-09 17:08:00 -0300

    Navegação entre telas

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,32 +4,19 @@ import '../models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    final meal = ModalRoute.of(context)!.settings.arguments as Meal;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredientes',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
-          Container(
-            width: 300,
+    );
+  }
+
+  Widget _createSectionContainer(Widget child) {
+    return Container(
+            width: 350,
             height: 200,
             padding: EdgeInsets.all(10),
             margin: EdgeInsets.all(10),
@@ -38,23 +25,67 @@ class MealDetailScreen extends StatelessWidget {
               border: Border.all(color: Colors.grey),
               borderRadius: BorderRadius.circular(10)
             ),
-            child: ListView.builder(
-              itemCount: meal.ingredients.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10,
-                    ),
-                    child: Text(meal.ingredients[index]),
-                  ),
-                  color: Theme.of(context).accentColor,
-                );
-              }
+            child: child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final meal = ModalRoute.of(context)!.settings.arguments as Meal;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          )
-        ],
+            _createSectionTitle(context, 'Ingredientes'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  );
+                },
+              ),
+            ),
+            _createSectionTitle(context, 'Passos'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(meal.steps[index]),
+                      ),
+                      Divider(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
