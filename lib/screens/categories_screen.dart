57de979458b7c73a34f7abd5d9b23aa5123c049b
Rodrigo Oliveira <rodrigoodeoliveira.santos@gmail.com>
2021-09-09 11:44:26 -0300

    adicionando componentes

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../components/category_item.dart';
+import '../componentes/category_item.dart';
 import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
