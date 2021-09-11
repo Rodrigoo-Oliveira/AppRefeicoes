3d382b0ca0ac7cc7ea8f1360dd4c6af58f276bd8
Rodrigo Oliveira <rodrigoodeoliveira.santos@gmail.com>
2021-09-11 09:24:52 -0300

    Marcando como favorito

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/categories_screen.dart';
-import 'categories_meals_screen.dart';
+
+import 'categories_screen.dart';
 import 'favorite_screen.dart';
 import '../components/main_drawer.dart';
-
+import '../models/meal.dart';
 
 class TabsScreen extends StatefulWidget {
+
+  final List<Meal> favoriteMeals;
+
+  const TabsScreen(this.favoriteMeals);
+
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
@@ -13,11 +18,19 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   
   int _selectScreenIndex = 0;
-  final List<Map<String, dynamic>> _screens = [
-    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
-    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()},
-  ];
+  late List<Map<String, dynamic>> _screens;
 
+  @override
+  void initState() {
+    super.initState();
+      _screens = [
+      {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+      {
+        'title': 'Meus Favoritos', 
+        'screen': FavoriteScreen(widget.favoriteMeals),
+      },
+    ];
+  }
   _selectScreen(int index) {
     setState(() {
       _selectScreenIndex = index;
